95dad52ea94915ff6a504ab3f6aba300d09ffa3d
webiumsk <info@webium.sk>
2026-01-29 00:48:00 +0100

    feat: Add standalone deployment configuration and enhance deploy script for automatic detection of compose file

---
+++ deploy.config.standalone.sh
@@ -0,0 +1,8 @@
+# satflux.io Standalone Deployment Configuration
+# Save this as deploy.config.sh on the server to use these settings
+
+COMPOSE_FILE="docker-compose.standalone.yml"
+PROJECT_NAME="satflux_standalone"
+
+# To use this with the deploy script, you can symlink it:
+# ln -sf deploy.config.standalone.sh deploy.config.sh
