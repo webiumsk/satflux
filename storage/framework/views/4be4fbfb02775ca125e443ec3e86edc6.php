d11244b3d3779f29489807cee561740304f077ec
webiumsk <info@webium.sk>
2026-03-28 19:55:00 +0100

    chore: clean up storage framework views by removing unused files and adding .gitignore
    - Deleted multiple unused view files from the storage/framework/views directory to reduce clutter and improve maintainability.
    - Added a .gitignore file in the storage/framework/views directory to exclude all files except the .gitignore itself, ensuring that only necessary files are tracked in version control.

--- storage/framework/views/4be4fbfb02775ca125e443ec3e86edc6.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH /home/peterhorvath/apps/bitcoin/D21Panel/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
