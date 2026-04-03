7273fb3695c5837bda86805589c20e6a33dbd031
webiumsk <info@webium.sk>
2026-04-03 12:26:49 +0200

    fix: update SVG icon in PointOfSaleShow component for improved clarity

--- resources/js/pages/stores/PointOfSaleShow.vue
+++ resources/js/pages/stores/PointOfSaleShow.vue
@@ -94,7 +94,7 @@
                           stroke-linecap="round"
                           stroke-linejoin="round"
                           stroke-width="2"
-                          d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                          d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"
                         />
                       </svg>
                     </div>
